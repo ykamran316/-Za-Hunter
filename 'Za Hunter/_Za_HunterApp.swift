d57d7eb2df384fd01ccb75939a789455966bb048
Yawar Kamran <yawarkamran316@gmail.com>
2021-03-08 10:44:24 -0600

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Student on 3/8/21.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
